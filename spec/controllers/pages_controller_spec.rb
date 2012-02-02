56f1626d7db56b82a9d1723ae9b5a5554c077ee8
Zeljko Velickovic <zzeljko.velickovicc@gmail.com>
2012-02-02 19:47:41 +0000

    2nd

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -15,5 +15,10 @@ describe PagesController do
       response.should be_success
     end
   end
-
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+  end
 end
